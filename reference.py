a1d3d06511c3b421616dc7476944c717fe4c5110
DPal83 <127270081+DPal83@users.noreply.github.com>
2023-11-16 17:41:15 -0500

    Update reference.py

--- reference.py
+++ reference.py
@@ -1,7 +1,112 @@
 import tkinter as tk
 from tkinter import messagebox, ttk
 from ttkbootstrap import Style
-from quiz_data import quiz_data
+
+
+quiz_data = [
+    {
+        "question": "Which Of these is a toothbrush?",
+        "choices": ["cloth", "pencil", "toothbrush", "stick"],
+        "answer": "toothbrush"
+    },
+    {
+        "question": " What is used to clean between your teeth?",
+        "choices": ["floss", "safety pin", "toothpick", "keys"],
+        "answer": "floss"
+    },
+    {
+        "question": " What should you use to protect your teeth when playing sports?",
+        "choices": ["helmet", "mouth gaurd", "bubble gum", "mask"],
+        "answer": "mouth gaurd"
+    },
+    {
+        "question": "Which picture represents a healthy snack for your teeth?",
+        "choices": ["apple", "sandwich", "candy", "gummy bear"],
+        "answer": "apple"
+    },
+    {
+        "question": "Which picture represents a healthy drink for your teeth?",
+        "choices": ["water", "milk", "soda", "sugary juice"],
+        "answer": "water"
+    },
+    {
+        "question": " Which picture shows a tooth with a cavity?",
+        "choices": ["China", "Japan", "South Korea", "Thailand"],
+        "answer": "Japan"
+    },
+    {
+        "question": "Which picture shows a toothbrushing technique?",
+        "choices": ["China", "Japan", "South Korea", "Thailand"],
+        "answer": "Japan"
+    },
+    {
+        "question": "Which picture represents a dental check-up?",
+        "choices": ["China", "Japan", "South Korea", "Thailand"],
+        "answer": "Japan"
+    },
+    {
+        "question": "What should you avoid doing with your teeth?",
+        "choices": ["brushing", "flossing", "mouth rinse", "opening bottle"],
+        "answer": "opening bottle"
+    },
+    {
+        "question": "What should you do when you first arrive at the dental clinic?",
+        "choices": ["Run around and explore","Sit quietly and wait for your turn","Yell and make noise"],
+        "answer": "Sit quietly and wait for your turn"
+    },
+    {
+        "question": "What might the dentist use to count your teeth during a check-up?",
+        "choices": ["Pencil", "Toothbrush", "Mirror and a small tool"],
+        "answer": "Mirror and a small tool"
+    },
+    {
+        "question": "How should you behave while sitting in the dental chair?",
+        "choices": ["Wiggle and squirm", "Sit still and listen to the dentist", "Jump up and down"],
+        "answer": "Sit still and listen to the dentist"
+    },
+    {
+        "question": "What can you do to be brave during a dental treatment?",
+        "choices": ["Cry loudly", "Hold the dentist's hand and take deep breaths", "Refuse to cooperate"],
+        "answer": "Hold the dentist's hand and take deep breaths"
+    },
+    {
+        "question": "What is the name of the machine that the dentist might use to take pictures of your teeth?",
+        "choices": ["Camera", "Microscope","X-ray machine"],
+        "answer": "X-ray machine"
+    },
+    {
+        "question": "What can you do if you feel scared at the dental clinic?",
+        "choices": ["Scream loudly", "Talk to the dentist about your feelings","Hide under the chair"],
+        "answer": "Talk to the dentist about your feelings"
+    },
+    {
+        "question": "What might the dentist use to fix a small hole in your tooth?",
+        "choices": ["Glue", "Toothpaste","Filling"],
+        "answer": "Filling"
+    },
+    {
+        "question": "What should you do if you accidentally bite the dentist's fingers during a treatment?",
+        "choices": ["Laugh loudly", "Apologize and try not to bite again"," Bite again on purpose"],
+        "answer": "Apologize and try not to bite again"
+    },
+    {
+        "question": "Why is it important to be honest with the dentist?",
+        "choices": ["To tell funny stories", "To keep secrets","So the dentist can help you better"],
+        "answer": "So the dentist can help you better"
+    },
+    {
+        "question": "How should you behave in the dental clinic's restroom?",
+        "choices": ["Flush the toilet and wash your hands", "Skip washing hands","Make a mess"],
+        "answer": "Flush the toilet and wash your hands"
+    },
+    {
+        "question": "How should you brush your teeth before going to the dentist?",
+        "choices": ["Quickly and without toothpaste", " Thoroughly with toothpaste","No need to brush your teeth before dental visit"],
+        "answer": " Thoroughly with toothpaste"
+    },
+    # Add more questions here
+]
+
 
 # Function to display the current question and choices
 def show_question():
